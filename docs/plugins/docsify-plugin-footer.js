10ec46211f33e3c0093200a6a7b0ce0ea9b13239
Brent Frymire <bfrymire22@gmail.com>
2024-02-12 19:59:02 -0500

    Fix edit page link on 404 errors

--- docs/plugins/docsify-plugin-footer.js
+++ docs/plugins/docsify-plugin-footer.js
@@ -1,6 +1,7 @@
 (function () {
     const footerPlugin = (hook, vm) => {
       hook.afterEach((html) => {
+        const is404 = html.includes('id="not-found"');
         const year = new Date().getFullYear() || 2020;
         const footerBegin = '<footer style="margin-top:3rem;"><p align="right">';
         const footerEnd = '</p></footer>';
@@ -9,7 +10,7 @@
           `Docs created using <a rel="nofollow" target="_blank" href="https://docsify.js.org/">Docsify</a>`,
         ];
         if (vm.route.file) {
-            const link = `//github.com/bfrymire/crispy/blob/docs/docs/${vm.route.file}`;
+            const link = `//github.com/bfrymire/crispy/blob/docs/docs/${is404 ? '_404.md' : vm.route.file}`;
             const editLink = `<a href="${link}" target="_blank">Edit this page on GitHub</a>`;
             footerItems.unshift(editLink);
           }
